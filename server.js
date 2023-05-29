622c4e5d5bf884401253eb1fd8b9034e6fd2997e
ZandroDiocampo <130553875+ZandroDiocampo@users.noreply.github.com>
2023-05-29 12:05:16 +0800

    edited

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 import express from 'express';
 import morgan from 'morgan';
-import cartRoutes from './routes/cart.js'
+import cartRoutes from './routes/cart.js';
 
 const app = express();
 
-const server = app.listen(8081, function () {
-  var host = server.address().address;
-  var port = server.address().port;
+app.use(express.json());
+app.use(morgan('dev'));
 
-  console.log("Example app listening at http://%s:%s", host, port);
-})
+// CORS middleware
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
+app.use('/api/cart', cartRoutes);
 
 app.get('/', (req, res) => {
-  res.send('Hello Worl3')
-})
-
-app.use(express.json())
-app.use(morgan('dev'))
-
-app.use('/api/cart', cartRoutes)
+  res.send('Hello World');
+});
 
 app.get('/hello', (req, res) => {
-  res.send('world')
-})
+  res.send('world');
+});
+
+const server = app.listen(8081, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+  console.log("Example app listening at http://%s:%s", host, port);
+});
